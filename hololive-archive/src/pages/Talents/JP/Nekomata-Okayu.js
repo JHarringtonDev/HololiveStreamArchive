871af25b49acc4e96d1dbdbbac9c32c399c90b4d
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-08-07 20:56:13 -0700

    removed strict mode and display results based on includes method

--- hololive-archive/src/pages/Talents/JP/Nekomata-Okayu.js
+++ hololive-archive/src/pages/Talents/JP/Nekomata-Okayu.js
@@ -4,19 +4,37 @@ import Header from "../../header";
 import Footer from "../../footer";
 import { Link } from "react-router";
 
-function fillResults(){
+let resultsArr = []
+let displayArr = []
+let videoDisplay
 
-    const sectionRef = useRef();
-    fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUvaTdHTWBGv3MKj3KVqJVCw&key=AIzaSyApDNwFNfi4HTJOEAVefxOdfBckc-pLqSs&maxResults=50")
-    .then(res => res.json())
-    .then(data=>{
-        data.items.forEach(e => {
-            sectionRef.innerHTML += `<a href="https://www.youtube.com/watch?v=${e.snippet.resourceId.videoId}"><img src=${e.snippet.thumbnails.medium.url}></a><br/>
-            <h3>${e.snippet.title}<br/>`
+function fillResults()
+    {
+        console.log("function ran")
+        fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUvaTdHTWBGv3MKj3KVqJVCw&key=AIzaSyApDNwFNfi4HTJOEAVefxOdfBckc-pLqSs&maxResults=50")
+        .then(res => res.json())
+        .then(data=>{
+            videoDisplay = document.querySelector("#results")
+            data.items.forEach(e => {
+                resultsArr.push(e)
+            })
+            //console.log(data);
         })
-        //console.log(data);
-    })
-}
+    }
+
+    function displayResults()
+    {
+        resultsArr.forEach((e) => 
+            {
+                if(e.snippet.title.includes("【 ドンキーコングバナンザ 】")){
+                    videoDisplay.innerHTML += `<a href="https://www.youtube.com/watch?v=${e.snippet.resourceId.videoId}"><img src=${e.snippet.thumbnails.medium.url}></a><br/>
+                    <h3>${e.snippet.title}<br/>`
+                }
+            } )
+            console.log(resultsArr.length)
+    }
+
+
 class Okayu extends React.Component{
     render(){
         fillResults()
@@ -33,14 +51,9 @@ class Okayu extends React.Component{
             <div id="main">
                 <div class="inner">
                     <header>
-                        <h1>This is Phantom, a free, fully responsive site<br />
-                        template designed by <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                        <p>Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem ipsum dolor sit amet nullam dolore.</p>
+                        <h1 onClick={() => {displayResults()}}>Donkey Kong Bananza</h1>
+                        <p id="results"></p>
                     </header>
-                    <section ref={sectionRef}></section>
-					
-                    
-
                 </div>
             </div>
 
